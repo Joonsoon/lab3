8de46188d60c8aab9ff19b72b63da80bf29caf10
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2024-02-20 08:32:40 +0800

    added mysqli in ci4

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -9,5 +9,15 @@ $routes->get('/', 'Home::index');
 
 use App\Controllers\Pages;
 
+use App\Controllers\News; // Add this line
+
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
+
