87ab14d9b3eeba6f5e35d87145b7d044a4e709c6
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2024-03-04 17:24:53 +0800

    added the create news item

--- ci4/app/Config/Filters.php
+++ ci4/app/Config/Filters.php
@@ -24,6 +24,8 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+    
+
     ];
 
     /**
@@ -56,7 +58,9 @@ class Filters extends BaseConfig
      * permits any HTTP method to access a controller. Accessing the controller
      * with a method you don't expect could bypass the filter.
      */
-    public array $methods = [];
+    public $methods = [
+        'post' => ['csrf'],
+    ];
 
     /**
      * List of filter aliases that should run on any
--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -8,16 +8,13 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 use App\Controllers\Pages;
-
 use App\Controllers\News; // Add this line
 
 
-$routes->get('news', [News::class, 'index']);           // Add this line
-$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']); // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']);
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
-
-$routes->get('pages', [Pages::class, 'index']);
-$routes->get('(:segment)', [Pages::class, 'view']);
-
